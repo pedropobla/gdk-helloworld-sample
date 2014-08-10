fc27708dc983d5cb362674882f50d71f915225c9
Pedro Perez Sanchez <pedropobla@gmail.com>
2014-08-10 03:14:08 -0400

    Adjusting indentation to 4 spaces

--- src/co/zerep/helloworld/MainActivity.java
+++ src/co/zerep/helloworld/MainActivity.java
@@ -25,33 +25,33 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends Activity {
-	
-	private static final String TAG = MainActivity.class.getSimpleName();
-	
-	private Card helloCard;
 
-	@Override
-	protected void onCreate(Bundle bundle) {
-		super.onCreate(bundle);
-		
-		Log.d(TAG, "Application launched.");
-		helloCard = new Card(this).setText(R.string.hello_world);
-		setContentView(helloCard.getView());
-	}
+    private static final String TAG = MainActivity.class.getSimpleName();
 
-	@Override
+    private Card helloCard;
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+        Log.d(TAG, "Application launched.");
+        helloCard = new Card(this).setText(R.string.hello_world);
+        setContentView(helloCard.getView());
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
-        
+
         Log.d(TAG, "Application resumed.");
         // onResume code here
     }
 
-	@Override
+    @Override
     protected void onPause() {
-		// onPause code here
-		Log.d(TAG, "Application paused.");
-		
+        // onPause code here
+        Log.d(TAG, "Application paused.");
+
         super.onPause();
-    }	
+    }
 }
